70a65f92319ade1e36a895b6231cba5bdcbcc353
DeveshDutt2710 <dd123.ues2017@gmail.com>
2021-07-26 23:53:43 +0530

    #149

---
+++ GFG/stacks/16_NGR_stackEffecient.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void NGR(int arr[], int n)
+{
+    vector<int> V;
+    stack<int> s;
+    
+    for(int i=n-1;i>=0;i--)
+    {
+        if(s.empty())
+        {
+            V.push_back(-1);
+        }
+        else
+        {
+            if(s.top()>arr[i])
+            {
+                V.push_back(s.top());
+            }
+            else
+            {
+                while(!s.empty())
+                {
+                    s.pop();
+                    if(s.empty())
+                    {
+                        V.push_back(-1);
+                    }
+                    else
+                    {
+                        V.push_back(s.top());
+                    }
+                }
+            }
+        }
+        s.push(arr[i]);
+    }
+    
+    reverse(V.begin(), V.end());
+    
+    for(int i=0;i<n;i++)
+    {
+        cout<<V[i]<<" ";
+    }
+}
+
+int main() {
+	int arr[] = {11, 13, 21, 3};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    
+    NGR(arr,n);
+	return 0;
+}
