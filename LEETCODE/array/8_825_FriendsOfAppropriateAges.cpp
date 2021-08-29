4fa7eaa50705400ed21344a0c4a136e232710ebd
DeveshDutt2710 <dd123.ues2017@gmail.com>
2021-08-29 23:05:53 +0530

    #184

---
+++ LEETCODE/array/8_825_FriendsOfAppropriateAges.cpp
@@ -0,0 +1,38 @@
+/*
+https://leetcode.com/problems/friends-of-appropriate-ages/
+
+825_FriendsOfAppropriateAges
+
+There are n persons on a social media website. You are given an integer array ages where ages[i] is the age of the ith person.
+
+A Person x will not send a friend request to a person y (x != y) if any of the following conditions is true:
+
+age[y] <= 0.5 * age[x] + 7
+age[y] > age[x]
+age[y] > 100 && age[x] < 100
+Otherwise, x will send a friend request to y.
+
+Note that if x sends a request to y, y will not necessarily send a request to x. Also, a person will not send a friend request to themself.
+
+Return the total number of friend requests made.
+*/
+
+
+class Solution {
+public:
+    int numFriendRequests(vector<int>& ages) {
+        unordered_map<int, int> count;
+        for (int &age : ages)
+            count[age]++;
+        int res = 0;
+        for (auto &a : count)
+            for (auto &b : count)
+                if (request(a.first, b.first))
+                    res += a.second * (b.second - (a.first == b.first ? 1 : 0));
+        return res;
+    }
+
+    bool request(int a, int b) {
+        return !(b <= 0.5 * a + 7 || b > a || (b > 100 && a < 100));
+    }
+};
